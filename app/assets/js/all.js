d86063b49e2b627d805d7e84a03e43abb4b22e06
tsai81926 <tsai81926@gmail.com>
2021-09-13 15:39:59 +0800

    0913version

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,10 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+/*DatePicker*/
+const elem = document.querySelector('input[name="datepicker"]');
+    const datepicker = new Datepicker(elem, {
+    nextArrow: '>',
+    prevArrow: '<',
+    });
