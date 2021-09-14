943053088f70a3d47157a9cdb019abb0fb32973d
tsai81926 <tsai81926@gmail.com>
2021-09-14 15:37:59 +0800

    indexAOS

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,10 +1,16 @@
+/*Default*/
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+/*AOS*/
+AOS.init();
 
 /*DatePicker*/
 const elem = document.querySelector('input[name="datepicker"]');
     const datepicker = new Datepicker(elem, {
     nextArrow: '>',
     prevArrow: '<',
-    });
+  });
+
+
