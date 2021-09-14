7134b87fdcee7a27685270a0189155da9159899d
tsai81926 <tsai81926@gmail.com>
2021-09-14 15:38:41 +0800

    Update 2021-09-14T07:38:39.239Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,8 +1,12 @@
 "use strict";
 
+/*Default*/
 $(function () {
   console.log('Hello Bootstrap5');
 });
+/*AOS*/
+
+AOS.init();
 /*DatePicker*/
 
 var elem = document.querySelector('input[name="datepicker"]');
